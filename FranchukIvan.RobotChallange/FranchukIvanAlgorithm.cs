413b30fa89a69f78b51baf9a1c6a357f20414ba3
sejum-x <franchukivan123@gmail.com>
2024-09-22 22:11:33 +0300

    new algo

--- FranchukIvan.RobotChallange/FranchukIvanAlgorithm.cs
+++ FranchukIvan.RobotChallange/FranchukIvanAlgorithm.cs
@@ -6,60 +6,94 @@ namespace FranchukIvan.RobotChallange
 {
     public class IvanFranchukAlgorithm : IRobotAlgorithm
     {
-        private int Fri = -1;
-        private int round = 0;
+        private int firstRobotIndex = -1;
+        private int currentRound = 0;
 
         public string Author => "Ivan Franchuk";
 
         public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
         {
-            if (Fri == -1)
-                Fri = robotToMoveIndex;
+            InitializeRound(robotToMoveIndex);
 
-            if (Fri == robotToMoveIndex)
-                ++round;
-
-            if (round == 51)
+            // На 51-му раунді робот збирає енергію
+            if (currentRound == 51)
                 return new CollectEnergyCommand();
 
             Robot.Common.Robot robot = robots[robotToMoveIndex];
 
-            if (round % 10 == 5)
-            {
-                var bestPosition = Functions.BestPositions(map, robot.Position, 100)
-                    .ToList()
-                    .Find(p => Functions.IsAvailablePosition(map, robots, p.Value, Author) &&
-                               robot.Energy > Functions.GetDistanceCost(robot.Position, p.Value));
-
-                if (bestPosition.Value != null)
-                {
-                    return new MoveCommand { NewPosition = bestPosition.Value };
-                }
-            }
-
-            if (Functions.GetAuthorRobotCount(robots, Author) < 100 && robot.Energy > 300)
+            if (ShouldCreateNewRobot(robots, robot))
                 return new CreateNewRobotCommand();
 
-            IList<EnergyStation> nearbyResources = map.GetNearbyResources(robot.Position, 2);
-            if (nearbyResources.Any(energyStation => round < 5 || energyStation.Energy > 100))
-            {
+            // Атакуємо, якщо є потенційні цілі
+            var attackCommand = TryAttack(robot, map, robots);
+            if (attackCommand != null)
+                return attackCommand;
+
+            // Збираємо енергію, якщо поруч є багато
+            if (HasEnoughEnergyNearby(robot, map))
                 return new CollectEnergyCommand();
-            }
 
-            var nextBestPosition = Functions.BestPositions(map, robot.Position, 5)
-                .ToList()
-                .Find(p => Functions.IsAvailablePosition(map, robots, p.Value, Author) &&
-                           robot.Energy > Functions.GetDistanceCost(robot.Position, p.Value));
-
-            if (nextBestPosition.Value != null)
-            {
-                return new MoveCommand { NewPosition = nextBestPosition.Value };
-            }
+            // Переміщуємо робота на найвигіднішу позицію
+            var moveCommand = TryMoveToBestPosition(robot, map, robots);
+            if (moveCommand != null)
+                return moveCommand;
 
             return new CollectEnergyCommand();
         }
 
+        private void InitializeRound(int robotToMoveIndex)
+        {
+            if (firstRobotIndex == -1)
+                firstRobotIndex = robotToMoveIndex;
+
+            if (firstRobotIndex == robotToMoveIndex)
+                ++currentRound;
+        }
+
+        private bool ShouldCreateNewRobot(IList<Robot.Common.Robot> robots, Robot.Common.Robot robot)
+        {
+            return Functions.GetAuthorRobotCount(robots, Author) < 100 && robot.Energy > 250;
+        }
+
+        private RobotCommand TryAttack(Robot.Common.Robot robot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            var potentialTargets = Functions.FindAttackTargets(map, robot.Position, robots, Author);
+            if (!potentialTargets.Any())
+                return null;
+
+            Position targetPosition = potentialTargets.First().Value;
+            int moveCost = Functions.GetDistanceCost(robot.Position, targetPosition);
+
+            if (robot.Energy > moveCost + 100)
+                return new MoveCommand { NewPosition = targetPosition };
+
+            return null;
+        }
+
+        private bool HasEnoughEnergyNearby(Robot.Common.Robot robot, Map map)
+        {
+            int totalEnergyNearby = map.GetNearbyResources(robot.Position, 2)
+                                     .Sum(station => station.Energy);
+            return totalEnergyNearby > 150;
+        }
+
+        private RobotCommand TryMoveToBestPosition(Robot.Common.Robot robot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            foreach (var positionRate in Functions.EvaluatePositionValue(map, robot.Position, robots, Author))
+            {
+                if (robot.Energy > Functions.GetDistanceCost(robot.Position, positionRate.Value) &&
+                    Functions.IsAvailablePosition(map, robots, positionRate.Value, Author))
+                {
+                    return new MoveCommand { NewPosition = positionRate.Value };
+                }
+            }
+
+            return null;
+        }
+
         public int Round { get; set; }
+
+        public string Description => throw new System.NotImplementedException();
 
         public IvanFranchukAlgorithm() => Logger.OnLogRound += LogRound;
 
--- FranchukIvan.RobotChallange/Functions.cs
+++ FranchukIvan.RobotChallange/Functions.cs
@@ -5,17 +5,24 @@ using System.Linq;
 
 namespace FranchukIvan.RobotChallange
 {
-    internal class Functions
+    public class Functions
     {
-        protected Functions() { }
-
         private const int NearbyRadius = 1;
+        private const int MapSize = 100;
+        private const int AttackRadius = 10;
+        private const int EnergyThreshold = 1200;
 
-        public static int GetDistanceCost(Position center, Position distant) =>
-            (center.X - distant.X) * (center.X - distant.X) + (center.Y - distant.Y) * (center.Y - distant.Y);
+        protected Functions() { }
 
         public static int GetAuthorRobotCount(IList<Robot.Common.Robot> robots, string author) =>
             robots.Count(robot => robot.OwnerName == author);
+
+        public static int GetDistanceCost(Position from, Position to)
+        {
+            int deltaX = from.X - to.X;
+            int deltaY = from.Y - to.Y;
+            return deltaX * deltaX + deltaY * deltaY;
+        }
 
         public static int GetNearbyRobotCount(IList<Robot.Common.Robot> robots, Position position) =>
             robots.Count(robot => IsWithinRadius(position, robot.Position, NearbyRadius));
@@ -23,82 +30,63 @@ namespace FranchukIvan.RobotChallange
         public static bool IsWithinRadius(Position center, Position point, int radius) =>
             Math.Abs(center.X - point.X) <= radius && Math.Abs(center.Y - point.Y) <= radius;
 
-        public static bool IsAvailablePosition(Map map, IList<Robot.Common.Robot> robots, Position position, string author)
+        public static bool IsAvailablePosition(Map map, IList<Robot.Common.Robot> robots, Position position, string author) =>
+            !IsOutOfBounds(position) && !robots.Any(robot => robot.Position.Equals(position) && robot.OwnerName == author);
+
+        public static List<KeyValuePair<int, Position>> FindAttackTargets(
+            Map map,
+            Position robotPosition,
+            IList<Robot.Common.Robot> robots,
+            string author)
         {
-            if (position.X > 100 || position.X < 0 || position.Y > 100 || position.Y < 0)
-                return false;
-
-            return !robots.Any(robot => robot.Position.Equals(position) && robot.OwnerName == author);
-        }
-
-        public static List<KeyValuePair<int, Position>> BestPositions(Map map, Position center, int radius)
-        {
-            int length = 2 * radius + 1;
-            int[,] energyGrid = new int[length, length];
-            int topLeftY = center.Y - radius;
-            int topLeftX = center.X - radius;
-
-            // Заповнюємо енергетичну матрицю
-            FillEnergyGrid(map, topLeftX, topLeftY, energyGrid, length);
-
-            // Оцінюємо позиції з урахуванням витрат на переміщення
-            var evaluatedPositions = EvaluatePositions(center, topLeftX, topLeftY, energyGrid, length);
-
-            // Сортуємо позиції за вигідністю
-            return SortPositionsByValue(evaluatedPositions);
-        }
-
-        // Заповнюємо енергетичну матрицю з урахуванням енергетичних станцій
-        private static void FillEnergyGrid(Map map, int topLeftX, int topLeftY, int[,] energyGrid, int length)
-        {
-            map.Stations
-                .Where(station => IsStationInRange(station, topLeftX, topLeftY, length))
-                .ToList()
-                .ForEach(station => AddStationEnergyToGrid(station, topLeftX, topLeftY, energyGrid, length));
-        }
-
-        // Перевіряємо, чи знаходиться станція в радіусі досліджуваної області
-        private static bool IsStationInRange(EnergyStation station, int topLeftX, int topLeftY, int length)
-        {
-            return station.Position.X >= topLeftX - 2 && station.Position.X < topLeftX + length + 2 &&
-                   station.Position.Y >= topLeftY - 2 && station.Position.Y < topLeftY + length + 2;
-        }
-
-        // Додаємо енергію станції до позицій в радіусі 2
-        private static void AddStationEnergyToGrid(EnergyStation station, int topLeftX, int topLeftY, int[,] energyGrid, int length)
-        {
-            int stationOffsetX = station.Position.X - topLeftX;
-            int stationOffsetY = station.Position.Y - topLeftY;
-
-            for (int offsetY = -2; offsetY <= 2; ++offsetY)
-            {
-                for (int offsetX = -2; offsetX <= 2; ++offsetX)
-                {
-                    int gridX = stationOffsetX + offsetX;
-                    int gridY = stationOffsetY + offsetY;
-
-                    if (gridX >= 0 && gridX < length && gridY >= 0 && gridY < length)
-                        energyGrid[gridY, gridX] += station.Energy;
-                }
-            }
-        }
-
-        // Оцінюємо всі позиції з урахуванням витрат на переміщення
-        private static List<KeyValuePair<int, Position>> EvaluatePositions(Position center, int topLeftX, int topLeftY, int[,] energyGrid, int length)
-        {
-            return Enumerable.Range(0, length)
-                .SelectMany(y => Enumerable.Range(0, length)
-                    .Select(x => new KeyValuePair<int, Position>(
-                        energyGrid[y, x] - GetDistanceCost(center, new Position(topLeftX + x, topLeftY + y)),
-                        new Position(topLeftX + x, topLeftY + y))))
+            var killRate = robots
+                .Where(robot => robot.OwnerName != author &&
+                                IsWithinRadius(robotPosition, robot.Position, AttackRadius) &&
+                                robot.Energy >= EnergyThreshold)
+                .Select(robot => new KeyValuePair<int, Position>(
+                    CalculateKillPriority(robotPosition, robot),
+                    robot.Position))
+                .OrderByDescending(k => k.Key)
                 .ToList();
+
+            return killRate;
         }
 
-        // Сортуємо позиції за спаданням
-        private static List<KeyValuePair<int, Position>> SortPositionsByValue(List<KeyValuePair<int, Position>> positions)
+        private static int CalculateKillPriority(Position from, Robot.Common.Robot targetRobot)
         {
-            positions.Sort((pair1, pair2) => -pair1.Key.CompareTo(pair2.Key));
-            return positions;
+            int distanceCost = GetDistanceCost(from, targetRobot.Position);
+            int energyImpact = (int)(targetRobot.Energy * 0.1);
+            return distanceCost + 30 - energyImpact;
         }
+
+        public static List<KeyValuePair<int, Position>> EvaluatePositionValue(
+            Map map,
+            Position robotPosition,
+            IList<Robot.Common.Robot> robots,
+            string author)
+        {
+            var ratedPositions = Enumerable
+                .Range(0, MapSize + 1)
+                .SelectMany(x => Enumerable.Range(0, MapSize + 1), (x, y) => new Position(x, y))
+                .Select(position =>
+                {
+                    if (!IsAvailablePosition(map, robots, position, author))
+                        return new KeyValuePair<int, Position>(0, position);
+
+                    int energyValue = GetTotalNearbyEnergy(map, position);
+                    int score = energyValue - GetDistanceCost(robotPosition, position);
+                    return new KeyValuePair<int, Position>(score, position);
+                })
+                .OrderByDescending(p => p.Key)
+                .ToList();
+
+            return ratedPositions;
+        }
+
+        private static int GetTotalNearbyEnergy(Map map, Position position) =>
+            map.GetNearbyResources(position, 2).Sum(station => station.Energy);
+
+        private static bool IsOutOfBounds(Position position) =>
+            position.X < 0 || position.X > MapSize || position.Y < 0 || position.Y > MapSize;
     }
 }
